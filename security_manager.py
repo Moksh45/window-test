ee901ba335771aefaebf5aa57de755c630c9786c
Moksh45 <mokshgupta567@gmail.com>
2023-08-24 12:22:41 +0530

    add comment

--- security_manager.py
+++ security_manager.py
@@ -4,6 +4,7 @@ import ctypes
 from winsdk.windows.devices import radios
 import os
 
+# Disable USB ports by modifying power management settings
 def block_usb_ports():
     # Get a list of all USB devices
     devices = os.listdir('/sys/bus/usb/devices')
@@ -13,42 +14,56 @@ def block_usb_ports():
         os.system('echo -n 0 > /sys/bus/usb/devices/{}/power/autosuspend'.format(device))
 
 
+# Disable Bluetooth radios asynchronously
 async def disable_bluetooth():
+    # Get a list of all available radios
     all_radios = await radios.Radio.get_radios_async()
+    
+    # Iterate through radios and turn off Bluetooth if available
     for this_radio in all_radios:
         if this_radio.kind == radios.RadioKind.BLUETOOTH:
             await this_radio.set_state_async(radios.RadioState.OFF)
 
 
+# Disable Command Prompt by modifying the Windows Registry
 def disable_command_prompt():
     try:
+        # Registry key and value information
         key = r"HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\System"
         value_name = "DisableCMD"
         value_data = 2  # 2 means disable
 
+        # Set the registry key value to disable Command Prompt
         ctypes.windll.advapi32.RegSetKeyValueW(None, key, value_name, ctypes.c_ulong(value_data), 4)
         print("Command Prompt disabled successfully.")
     except Exception as e:
         print("Error:", e)
 
 
+# Block access to specific websites by modifying the hosts file
 def block_websites():
-    hosts_path = r"C:\Windows\System32\drivers\etc\hosts"  # Path for Windows
-    redirect = "127.0.0.1"
+    # Path to the hosts file on Windows
+    hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+    redirect = "127.0.0.1"  # Loopback IP address
     website_list = ["www.facebook.com", "facebook.com"]
 
+    # Open the hosts file for reading and writing
     with open(hosts_path, 'r+') as file:
         content = file.read()
         for website in website_list:
             if website in content:
                 print(f"{website} is already blocked.")
             else:
+                # Write redirect entry to block the website
                 file.write(redirect + " " + website + "\n")
                 print(f"{website} is now blocked.")
     print("Blocking completed.")
 
+
+# Main entry point of the script
 if __name__ == "__main__":
-    block_usb_ports()
-    asyncio.run(disable_bluetooth())
-    disable_command_prompt()
-    block_websites()
+    # Call functions to perform the desired tasks
+    block_usb_ports()        # Disable USB ports
+    asyncio.run(disable_bluetooth())  # Disable Bluetooth radios
+    disable_command_prompt()          # Disable Command Prompt
+    block_websites()         # Block specific websites
